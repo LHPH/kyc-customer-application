ed1d983c4c0af7f17088bbc543faf99893938e12
LHPH <luislyly@hotmail.com>
2025-05-21 12:59:39 -0600

    Upload adjust to have the flow in executive-document service

--- src/api/services/kyc-reports.service.ts
+++ src/api/services/kyc-reports.service.ts
@@ -3,6 +3,8 @@ import RequestData from "src/common/interfaces/request-data";
 import { DiscoveryService } from "src/eureka/discovery/discovery.service";
 import { ApplicationFormRequest } from "../document/interfaces/application-form";
 import { ContractRequest } from "../document/interfaces/contract";
+import { ReportResponse } from "../document/interfaces/kyc-reports-response";
+import ResponseData from "src/common/interfaces/response-data";
 
 
 @Injectable()
@@ -12,14 +14,34 @@ export class KycReportService{
 
     constructor(discoveryService: DiscoveryService){}
 
-    async createApplicationFormDocument(request: RequestData<ApplicationFormRequest>){
+    async createApplicationFormDocument(request: RequestData<ApplicationFormRequest>): Promise<ResponseData<ReportResponse>>{
 
         this.logger.log('Create Document '+JSON.stringify(request.data));
+
+        return Promise.resolve( {
+            data: {
+                id: '1-IDGF',
+                name: '',
+                mimeType: 'm',
+                size: 0,
+                date: ''
+            },
+        });
     }
 
-    async createContractDocument(request: RequestData<ContractRequest>){
+    async createContractDocument(request: RequestData<ContractRequest>): Promise<ResponseData<ReportResponse>>{
 
         this.logger.log('Create contract '+JSON.stringify(request.data));
+
+        return Promise.resolve( {
+            data: {
+                id: '1-IDGF',
+                name: '',
+                mimeType: 'm',
+                size: 0,
+                date: ''
+            },
+        });
     }
 
 }
--- src/executives/services/executive-document.service.ts
+++ src/executives/services/executive-document.service.ts
@@ -2,7 +2,7 @@ import { HttpStatus, Injectable } from "@nestjs/common";
 
 import { KycMessagesService } from "src/common/services/kyc-message.service";
 import Message  from "src/common/interfaces/message";
-import { GetDocumentRequest, GetDocumentResponse } from "../interfaces/get-documents";
+import { DocumentData, GetDocumentRequest, GetDocumentResponse } from "../interfaces/get-documents";
 import RequestData from "src/common/interfaces/request-data";
 import { KycReportService } from "src/api/services/kyc-reports.service";
 import KycCustomerApplicationEntity from "src/common/entities/kyc-customer-application";
@@ -14,6 +14,8 @@ import KycCustomerServiceEntity from "src/common/entities/kyc-customer-service";
 import { ContractRequest } from "src/api/document/interfaces/contract";
 import { MessageCodes } from "src/common/enums/message-codes";
 import { KycRestException } from "src/common/exception/kyc-rest-exception.exception";
+import ResponseData from "src/common/interfaces/response-data";
+import { ReportResponse } from "src/api/document/interfaces/kyc-reports-response";
 
 @Injectable()
 export default class ExecutiveDocumentService{
@@ -29,7 +31,7 @@ export default class ExecutiveDocumentService{
         const data: GetDocumentRequest = requestData.data!;
         const auth = requestData.auth;
 
-        return this.kycCustomerApplicationRepository.find(
+        const folios: KycCustomerApplicationEntity[] = await this.kycCustomerApplicationRepository.find(
             {
                 relations:{
                     services: true
@@ -47,70 +49,71 @@ export default class ExecutiveDocumentService{
                     id: "DESC"
                 },
                 take: 1
-            })
-            .then(results => {
-
-                if(!results.length){
-
-                    const notification: Message = this.kycMessagesService.getMessage(MessageCodes.FOLIO_NOT_FOUND);
-                    throw new KycRestException({message: notification, status: HttpStatus.UNPROCESSABLE_ENTITY });
-                }
-
-                results.map(result => {
-
-                    const contractedServices: KycCustomerServiceEntity[] = result.services;
-
-                    let total = 0;
-                    const services: Service[] = contractedServices.map(contractedService => {
-
-                        total = total + contractedService.serviceCost;
-                        return {
-                            id: contractedService.service.id,
-                            cost: contractedService.serviceCost,
-                            name: contractedService.service.name
-                        }
-                    });
-
-                    const customerName = `${result.customer.firstName} ${result.customer.secondName || ''}` 
-                            +`${result.customer.lastName} ${result.customer.secondLastName || ''}`.trim();
-
-                    const customerAddress = `${result.customer.address.street} ${result.customer.address.streetNumber}, ${result.customer.address.postalCode}`;
-
-                    const applicationFormRequest: ApplicationFormRequest = {
-                        folio: result.id!,
-                        dateApplication: result.creationDate?.toISOString()!,
-                        total,
-                        campaign: result.offer?.campaign?.id,
-                        customerNumber: result.customer.id,
-                        customerName,
-                        customerAddress,
-                        customerPhone: result.customer.cellPhone,
-                        customerEmail: result.customer.email,
-                        customerRfc: result.customer.rfc,
-                        executiveName: `${result.executive.firstName} ${result.executive.lastName}`,
-                        idExecutive: result.executive.id,
-                        idBranch: result.office.id,
-                        branchName: result.office.name,
-                        acceptPromotions: result.promotions.acceptPromotions,
-                        acceptPromotionsEmail: result.promotions.acceptPromotionsEmail,
-                        acceptPromotionsCellPhone: result.promotions.acceptPromotionsCellPhone,
-                        services
-                    }
-
-                    const contractRequest: ContractRequest = {
-                        folio: result.id!,
-                        customerNumber: result.customer.id,
-                        customerName,
-                        customerAddress,
-                        contractedServices: services
-                    }
-
-                    this.kycReportService.createApplicationFormDocument({data: applicationFormRequest, auth});
-                    this.kycReportService.createContractDocument({data: contractRequest, auth});
-
-                });
-
-                return {documents: []};
             });
+
+            if(!folios.length){
+
+                const notification: Message = this.kycMessagesService.getMessage(MessageCodes.FOLIO_NOT_FOUND);
+                throw new KycRestException({message: notification, status: HttpStatus.UNPROCESSABLE_ENTITY });
+            }
+
+            const result: KycCustomerApplicationEntity = folios[0];
+            const contractedServices: KycCustomerServiceEntity[] = result.services;
+            let total = 0;
+
+            const services: Service[] = contractedServices.map(contractedService => {
+                total = total + contractedService.serviceCost;
+                return {
+                    id: contractedService.service.id,
+                    cost: contractedService.serviceCost,
+                    name: contractedService.service.name
+                }
+            });
+
+            const customerName = `${result.customer.firstName} ${result.customer.secondName || ''}` 
+                    +`${result.customer.lastName} ${result.customer.secondLastName || ''}`.trim();
+            const customerAddress = `${result.customer.address.street} ${result.customer.address.streetNumber}, ${result.customer.address.postalCode}`;
+            
+            const applicationFormRequest: ApplicationFormRequest = {
+                folio: result.id!,
+                dateApplication: result.creationDate?.toISOString()!,
+                total,
+                campaign: result.offer?.campaign?.id,
+                customerNumber: result.customer.id,
+                customerName,
+                customerAddress,
+                customerPhone: result.customer.cellPhone,
+                customerEmail: result.customer.email,
+                customerRfc: result.customer.rfc,
+                executiveName: `${result.executive.firstName} ${result.executive.lastName}`,
+                idExecutive: result.executive.id,
+                idBranch: result.office.id,
+                branchName: result.office.name,
+                acceptPromotions: result.promotions.acceptPromotions,
+                acceptPromotionsEmail: result.promotions.acceptPromotionsEmail,
+                acceptPromotionsCellPhone: result.promotions.acceptPromotionsCellPhone,
+                services
+            }
+
+            const contractRequest: ContractRequest = {
+                folio: result.id!,
+                customerNumber: result.customer.id,
+                customerName,
+                customerAddress,
+                contractedServices: services
+            }
+            const promiseApplicationForm: Promise<ResponseData<ReportResponse>> = this.kycReportService.createApplicationFormDocument({data: applicationFormRequest, auth});
+            const promiseContract: Promise<ResponseData<ReportResponse>> = this.kycReportService.createContractDocument({data: contractRequest, auth});
+            
+            const promisesResponse = await Promise.all([promiseApplicationForm,promiseContract]);
+            const documents: DocumentData[] = promisesResponse.map(response => {
+                const document: DocumentData = {
+                    id: response.data?.id!,
+                    name: response.data?.name!
+                }
+                return document;
+            })
+            const documentResponse: GetDocumentResponse = {documents}
+            return documentResponse;
     }
 }
